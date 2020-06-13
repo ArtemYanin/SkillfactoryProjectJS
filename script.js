1a759752266b81a230c7a32ca3bad0d7f0a8e47f
ArtemYanin <yanin1998@mail.ru>
2020-06-13 23:39:56 +0300

    Second commit
    Optimized script and fixed html

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 'use strict';
 
-let data = [
-	{
-		city: 'Rostov-on-Don LCD admiral',
-		apartmentArea: '81 m2',
-		repairTime: '3.5 months',
-		repairCost: 'Upon request',
-		img: './image/image0.png',
-	},
-	{
-		city: 'Sochi Thieves',
-		apartmentArea: '105 m2',
-		repairTime: '4 months',
-		repairCost: 'Upon request',
-		img: './image/image1.png',
-	},
-	{
-		city: 'Rostov-on-Don Patriotic',
-		apartmentArea: '93 m2',
-		repairTime: '3 months',
-		repairCost: 'Upon request',
-		img: './image/image2.png',
-	},
-];
-
 let leftArrow = document.querySelector('#leftArrow');
 let paragraph0div = document.querySelector('#paragraph0div');
 let paragraph0A = document.querySelector('#paragraph0A');
@@ -41,26 +17,48 @@ let index = 0;
 let styleAArr = document.querySelectorAll('.styleA');
 let contLeftContArrowChoiceParagraphArr = document.querySelectorAll('.contLeft_ContArrow_ChoiceParagraph');
 
-paragraph0div.addEventListener('click', activateParagraph0);
-paragraph0A.addEventListener('click', activateParagraph0);
-paragraph1div.addEventListener('click', activateParagraph1);
-paragraph1A.addEventListener('click', activateParagraph1);
-paragraph2div.addEventListener('click', activateParagraph2);
-paragraph2A.addEventListener('click', activateParagraph2);
+let data = [
+	{
+		city: 'Rostov-on-Don LCD admiral',
+		apartmentArea: '81 m2',
+		repairTime: '3.5 months',
+		repairCost: 'Upon request',
+		img: './image/image0.png',
+		idDiv: paragraph0div,
+		idA: paragraph0A,
+	},
+	{
+		city: 'Sochi Thieves',
+		apartmentArea: '105 m2',
+		repairTime: '4 months',
+		repairCost: 'Upon request',
+		img: './image/image1.png',
+		idDiv: paragraph1div,
+		idA: paragraph1A,
+	},
+	{
+		city: 'Rostov-on-Don Patriotic',
+		apartmentArea: '93 m2',
+		repairTime: '3 months',
+		repairCost: 'Upon request',
+		img: './image/image2.png',
+		idDiv: paragraph2div,
+		idA: paragraph2A,
+	},
+];
+
+
+for (let i = 0; i < data.length; i++) {
+	data[i].idDiv.addEventListener('click', () => mainExecutiveFunction(data[i].idDiv, data[i].idA, i));
+	data[i].idA.addEventListener('click', () => mainExecutiveFunction(data[i].idDiv, data[i].idA, i));
+}
 leftArrow.addEventListener('click', leftArrowEvent);
 rightArrow.addEventListener('click', rightArrowEvent);
 
-
-function activateParagraph0() {
-	mainExecutiveFunction(paragraph0div, paragraph0A, 0);
-}
-
-function activateParagraph1() {
-	mainExecutiveFunction(paragraph1div, paragraph1A, 1);
-}
-
-function activateParagraph2() {
-	mainExecutiveFunction(paragraph2div, paragraph2A, 2);
+function mainExecutiveFunction(idDiv, idA, index) {
+	clearOldActives();
+	addClass(idDiv, idA);
+	changesOnThePage(index);
 }
 
 function leftArrowEvent() {
@@ -101,10 +99,4 @@ function rightArrowEvent() {
 		pRepTime.innerHTML = data[num].repairTime;
 		pRepCost.innerHTML = data[num].repairCost;
 		image.src = data[num].img;
-	}
-
-	function mainExecutiveFunction(idDiv, idA, index) {
-		clearOldActives();
-		addClass(idDiv, idA);
-		changesOnThePage(index);
 	}
